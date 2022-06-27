5828deba4327e64a04b6a4a57f219743af4c6765
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-06-27 12:42:02 +0900

    threejs study-project init

---
+++ 02/basic/src/main.js
@@ -0,0 +1,13 @@
+import * as THREE from "three";
+
+/* 동적으로 캔버스 조립하기 
+// set renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.domElement : renderer가 갖고 있는 캔버스
+document.body.appendChild(renderer.domElement); // dom 조립 또는 html 내 canvas 태그 내 붙이기 (활용범위가 더 넓음)
+*/
+
+const canvas = document.getElementById("three-canvas");
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);

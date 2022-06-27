c7b934babedc76926739f4515d7fa095c213c743
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-06-27 13:48:47 +0900

    add scene and camera

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -11,3 +11,17 @@ document.body.appendChild(renderer.domElement); // dom 조립 또는 html 내 ca
 const canvas = document.getElementById("three-canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75, // fov(field of view, 시야각)
+  window.innerWidth / window.innerHeight, // aspect(종횡비)
+  0.1, // near
+  1000 // far
+);
+
+camera.position.z = 5;
+scene.add(camera);

75eb5cb1eaa96e63a3f47b1fd82a094629c8885d
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-06-27 14:06:04 +0900

    howto set OrthoGraphic Camera

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -15,16 +15,35 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(
-  75, // fov(field of view, 시야각)
-  window.innerWidth / window.innerHeight, // aspect(종횡비)
-  0.1, // near
-  1000 // far
+// const camera = new THREE.PerspectiveCamera(
+//   75, // fov(field of view, 시야각)
+//   window.innerWidth / window.innerHeight, // aspect(종횡비)
+//   0.1, // near
+//   1000 // far
+// );
+
+// camera.position.x = 1;
+// camera.position.z = 5;
+// camera.position.y = 2;
+
+/* Orthographic Camera 직교 카메라 - 롤 같은 게임에서 보는 뷰 */
+const camera = new THREE.OrthographicCamera(
+  -(window.innerWidth / window.innerHeight), // 절두체의 left
+  window.innerWidth / window.innerHeight, // right
+  1, // top
+  -1, // bottom
+  0.1,
+  1000
 );
 
 camera.position.x = 1;
-camera.position.z = 5;
 camera.position.y = 2;
+camera.position.z = 5; // z를 조정하면 perspective camera 와 다름, zoom을 조정 (+updateProjectMatrix )
+
+camera.lookAt(0, 0, 0); // xyz로 정의된 position을 바라봄
+camera.zoom = 0.5; // zoom out
+camera.updateProjectionMatrix(); // 카메라 속성 update
+
 scene.add(camera);
 
 // Mesh

9671e33cb2b8b7789b5ba80fab41988009e30c5c
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-07-02 22:38:19 +0900

    rotation-reorder method

--- 04/transform/src/ex03.js
+++ 04/transform/src/ex03.js
@@ -58,19 +58,25 @@ export default function example() {
   // 그리기
   const clock = new THREE.Clock();
 
+  // * rotation 속성 주의할 점 - 축을 바꿔주지 않으면 우리가 상상하는 입체 움직임 각도와 불일치
+  // 마인크래프트 게임캐릭터로 생각해보면 이해가 쉽다.
+  mesh.rotation.reorder("YXZ");
+  mesh.rotation.y = THREE.MathUtils.degToRad(45);
+  mesh.rotation.x = THREE.MathUtils.degToRad(20);
+
   function draw() {
     const delta = clock.getDelta();
 
-    mesh.position.set(-1, 2, -5);
+    // mesh.position.set(-1, 2, -5);
 
-    mesh.scale.set(4, 1, 2);
+    // mesh.scale.set(4, 1, 2);
 
     // * rotation
     // mesh.rotation.x = degToRad(45); // THREE.MathUtils.degToRad()
     // mesh.rotation.x = Math.PI / 4; // 180 / 4 == 45
     // mesh.rotation.x = 1; // PI기준 대비값. PI(3.14)의 1/3이므로 60도 정도
 
-    mesh.rotation.z += delta; // 회전하는 애니메이션 효과
+    // mesh.rotation.z += delta; // 회전하는 애니메이션 효과
 
     renderer.render(scene, camera);
     renderer.setAnimationLoop(draw);

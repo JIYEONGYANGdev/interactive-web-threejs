29999555265c94e0d24cf5e7ef34f2aba499b0ae
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-07-03 19:03:43 +0900

    setting for random meshes

--- 06/controls/src/ex01.js
+++ 06/controls/src/ex01.js
@@ -1,69 +1,86 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // ----- 주제: OrbitControls
 
 export default function example() {
-	// Renderer
-	const canvas = document.querySelector('#three-canvas');
-	const renderer = new THREE.WebGLRenderer({
-		canvas,
-		antialias: true
-	});
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-	// Scene
-	const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene();
 
-	// Camera
-	const camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1000
-	);
-	camera.position.y = 1.5;
-	camera.position.z = 4;
-	scene.add(camera);
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
 
-	// Light
-	const ambientLight = new THREE.AmbientLight('white', 0.5);
-	scene.add(ambientLight);
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
 
-	const directionalLight = new THREE.DirectionalLight('white', 1);
-	directionalLight.position.x = 1;
-	directionalLight.position.z = 2;
-	scene.add(directionalLight);
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
 
-	// Controls
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement); // 2nd argument: canvas object
 
-	// Mesh
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshStandardMaterial({
-		color: 'seagreen'
-	});
-	const mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+  // Mesh - 랜덤한 컬러(material), 랜덤한 위치로 다수 생성
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      // 최소값 50 설정해서 까만배경보다는 다 눈에 보이도록
+      color: `rgb(
+					${50 + Math.floor(Math.random() * (255 - 50))}, 
+					${50 + Math.floor(Math.random() * (255 - 50))},
+					${50 + Math.floor(Math.random() * (255 - 50))}
+				)
+				`,
+    });
+    mesh = new THREE.Mesh(geometry, material);
 
-	// 그리기
-	const clock = new THREE.Clock();
+    mesh.position.x = (Math.random() - 0.5) * 5; // -2.5 ~ 2.5
+    mesh.position.y = (Math.random() - 0.5) * 5;
+    mesh.position.z = (Math.random() - 0.5) * 5;
 
-	function draw() {
-		const delta = clock.getDelta();
+    scene.add(mesh);
+  }
 
-		renderer.render(scene, camera);
-		renderer.setAnimationLoop(draw);
-	}
+  // 그리기
+  const clock = new THREE.Clock();
 
-	function setSize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.render(scene, camera);
-	}
+  function draw() {
+    const delta = clock.getDelta();
 
-	// 이벤트
-	window.addEventListener('resize', setSize);
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
 
-	draw();
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
 }

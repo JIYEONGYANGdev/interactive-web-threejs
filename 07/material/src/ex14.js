294b252395dc4ce98a05be992d0d3c05e1689b56
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-07-10 13:50:20 +0900

    materials-EnvironmentMap(mirror image), w. cubeTextureLoader

---
+++ 07/material/src/ex14.js
@@ -0,0 +1,89 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// ----- 주제: EnvironmentMap, 주변이 Mesh에 비침(반사됨) 거울같은 물체에 맵핑
+
+export default function example() {
+  // 텍스처(이미지) 로드 - cubeMap
+  const cubeTextureLoader = new THREE.CubeTextureLoader();
+  const environmentTexture = cubeTextureLoader
+    .setPath("/textures/cubeMap/") // 경로를 먼저 설정
+    .load([
+      // +(p) -(n) 순서로
+      "px.png",
+      "nx.png",
+      "py.png",
+      "ny.png",
+      "pz.png",
+      "nz.png",
+    ]);
+
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color("white");
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.set(1, 1, 2);
+
+  scene.add(ambientLight, directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+
+  // const material = new THREE.MeshBasicMaterial({
+  const material = new THREE.MeshStandardMaterial({
+    envMap: environmentTexture,
+    metalness: 2,
+    roughness: 0.1,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
